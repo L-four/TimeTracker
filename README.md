106e48ef5eb905e1ab1536cb5fdb937157e11633
Kyah Rindlisbacher <kyah101@gmail.com>
2017-02-01 18:33:26 +1100

    init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+```bash
+# Install dependencies
+npm install
+#install angular-cli
+npm install -g angular-cli
+# build the app
+pushd angular
+ng build
+popd
+# Run the app
+npm start
+```
---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { MaterialModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    MaterialModule.forRoot(),
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
